83ed75388436bc8b763296ca4fb4cddee08cf2b9
dyh_a <xinzon32177@163.com>
2023-06-08 04:36:19 +0800

    feat(components/button): 透出Button的ref，使它可以外部赋予ref

--- packages/components/src/button/Button.tsx
+++ packages/components/src/button/Button.tsx
@@ -11,66 +11,69 @@ import './Button.module.scss';
 
 const cClass = getComponentClass('button');
 
-export const Button: React.FC<ButtonProps> = (props) => {
-  const {
-    className,
-    htmlType,
-    onClick,
-    plain,
-    size,
-    type,
-    children,
-    shape,
-    ...rest
-  } = props;
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const {
+      className,
+      htmlType,
+      onClick,
+      plain,
+      size,
+      type,
+      children,
+      shape,
+      ...rest
+    } = props;
 
-  const [active, setActive] = useState(false);
-  const waveRef = useRef<HTMLElement>(null);
+    const [active, setActive] = useState(false);
+    const waveRef = useRef<HTMLElement>(null);
 
-  useEffect(() => {
-    const el = waveRef.current;
-    if (!active || !el) return;
-    const cancel = () => el.removeEventListener('animationend', handler);
-    const handler = () => {
-      setActive(false);
-      cancel();
+    useEffect(() => {
+      const el = waveRef.current;
+      if (!active || !el) return;
+      const cancel = () => el.removeEventListener('animationend', handler);
+      const handler = () => {
+        setActive(false);
+        cancel();
+      };
+      el.addEventListener('animationend', handler);
+
+      return cancel;
+    }, [active]);
+
+    const clickHandler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+      if (props.disabled) return;
+      if (plain !== 'text') setActive(true);
+      return onClick?.(e);
     };
-    el.addEventListener('animationend', handler);
 
-    return cancel;
-  }, [active]);
-
-  const clickHandler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    if (props.disabled) return;
-    if (plain !== 'text') setActive(true);
-    return onClick?.(e);
-  };
-
-  return (
-    <button
-      {...rest}
-      type={htmlType}
-      onClick={clickHandler}
-      className={getClassNames(cClass, `type-${type}`, {
-        [CLASS_SIZE_SM]: size === 'small',
-        [CLASS_SIZE_M]: size === 'medium',
-        [CLASS_SIZE_LG]: size === 'large',
-        [`shape-round`]: shape === 'round',
-        [`shape-circle`]: shape === 'circle',
-        [`plain`]: plain === true,
-        [`plain-text`]: plain === 'text',
-        [`plain-dashed`]: plain === 'dashed',
-        [className as string]: className,
-      })}>
-      {children}
-      <span
-        ref={waveRef}
-        className={getClassNames('btn-wave', {
-          'btn-wave-active': active,
-        })}></span>
-    </button>
-  );
-};
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        type={htmlType}
+        onClick={clickHandler}
+        className={getClassNames(cClass, `type-${type}`, {
+          [CLASS_SIZE_SM]: size === 'small',
+          [CLASS_SIZE_M]: size === 'medium',
+          [CLASS_SIZE_LG]: size === 'large',
+          [`shape-round`]: shape === 'round',
+          [`shape-circle`]: shape === 'circle',
+          [`plain`]: plain === true,
+          [`plain-text`]: plain === 'text',
+          [`plain-dashed`]: plain === 'dashed',
+          [className as string]: className,
+        })}>
+        {children}
+        <span
+          ref={waveRef}
+          className={getClassNames('btn-wave', {
+            'btn-wave-active': active,
+          })}></span>
+      </button>
+    );
+  },
+);
 
 Button.defaultProps = {
   size: 'medium',

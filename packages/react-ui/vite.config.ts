a6fa5b3eb33785ca64b39169cde9cbca7437d11b
dyh_a <xinzon32177@163.com>
2023-06-12 16:47:58 +0800

    chore: vite打包添加banner

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -2,8 +2,25 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import Path, { resolve } from 'path';
 import Fs from 'fs';
+import pkg from './package.json';
 
 const pkgs = Fs.readdirSync(Path.resolve(__dirname, '../../packages'));
+
+function getBanner(format: string) {
+  const date = new Date();
+  return (
+    '/*!\n' +
+    ` * ToolPackReactUI(${pkg.name}) v${pkg.version}\n` +
+    ` * Author: ${pkg.author}\n` +
+    ` * Documentation: ${pkg.homepage}\n` +
+    ` * License: ${pkg.license}\n` +
+    ` * File: index.${format}.js\n` +
+    ` * Date: ${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}\n` +
+    ` */\n`
+  );
+}
 
 const entryPath = resolve(__dirname, './src/index.ts');
 /**
@@ -49,15 +66,18 @@ export default defineConfig(() => {
               react: 'React',
               'react-dom': 'ReactDom',
             },
+            banner: getBanner('iife'),
           },
           {
             format: 'es',
             entryFileNames: '[name].es.js',
+            banner: getBanner('es'),
           },
 
           {
             format: 'cjs',
             entryFileNames: '[name].cjs.js',
+            banner: getBanner('cjs'),
           },
         ],
       },

c9ce93b6aa8f383a5518b7e5f48ba2d0e50d882c
dyh_a <xinzon32177@163.com>
2023-06-14 14:58:52 +0800

    style: stylelint调整

--- internal/playground/src/app/App.tsx
+++ internal/playground/src/app/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">

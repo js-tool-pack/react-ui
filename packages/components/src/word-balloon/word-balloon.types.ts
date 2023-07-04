e242dd44c48755c6482b6df02e123ab3063a3305
dyh_a <xinzon32177@163.com>
2023-07-04 12:31:34 +0800

    fix(components/word-balloon): ref类型报错

--- packages/components/src/word-balloon/word-balloon.types.ts
+++ packages/components/src/word-balloon/word-balloon.types.ts
@@ -1,10 +1,11 @@
 import React from 'react';
 import { PLACEMENTS_12 } from '@pkg/shared';
 
-export type WordBalloonProps = React.HTMLAttributes<HTMLElement> & {
+export type WordBalloonProps = React.HTMLAttributes<HTMLDivElement> & {
   placement?: (typeof PLACEMENTS_12)[number];
   contentStyle?: React.CSSProperties;
   arrowStyle?: React.CSSProperties;
   background?: React.CSSProperties['background'];
   showArrow?: boolean;
+  ref?: React.Ref<HTMLDivElement>;
 };

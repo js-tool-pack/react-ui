52a944d413fa85ad83e91d464a6f04a446b0dd22
dyh_a <xinzon32177@163.com>
2023-07-12 18:49:25 +0800

    refactor(components/pop-confirm): 重构PopConfirm组件，使组件依赖从Tooltip改为Popover

--- packages/components/src/pop-confirm/pop-confirm.types.ts
+++ packages/components/src/pop-confirm/pop-confirm.types.ts
@@ -1,12 +1,11 @@
 import React from 'react';
-import { TooltipProps } from '../tooltip';
+import type { PopoverProps } from '../popover';
 
 export type PopConfirmProps = Omit<
   React.HTMLAttributes<HTMLElement>,
   'children'
 > &
-  TooltipProps & {
-    content?: React.ReactNode;
+  PopoverProps & {
     confirmText?: string | null;
     onConfirm?: () => boolean | Promise<void> | void;
     cancelText?: string | null;

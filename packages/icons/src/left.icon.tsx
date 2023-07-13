62618ec24636de2d3f3822523ee8fc02e73bf8c4
dyh_a <xinzon32177@163.com>
2023-07-13 21:02:36 +0800

    feat(icons): 新增各种方向的箭头icon

---
+++ packages/icons/src/left.icon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Left: React.FC = () => (
+  <svg viewBox="0 0 1024 1024">
+    <path
+      d="M659.748571 245.272381l-51.687619-51.687619-318.439619 318.585905 318.415238 318.268952 51.712-51.736381-266.703238-266.556952z"
+      fill="currentColor"></path>
+  </svg>
+);

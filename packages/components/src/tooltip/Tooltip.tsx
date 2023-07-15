dd7e3a3bc0c639662d80407c4518c04f363a1b43
dyh_a <xinzon32177@163.com>
2023-07-15 23:20:26 +0800

    refactor(components/tooltip): tooltip内部代码样式全部复用popover

--- packages/components/src/tooltip/Tooltip.tsx
+++ packages/components/src/tooltip/Tooltip.tsx
@@ -1,107 +1,17 @@
-import React, { useCallback, useRef } from 'react';
+import React from 'react';
 import type { TooltipProps } from './tooltip.types';
-import { getComponentClass, useResizeEvent } from '@pkg/shared';
-import type { RequiredPart } from '@tool-pack/types';
-import { getClassNames } from '@tool-pack/basic';
-import { WordBalloon } from '../word-balloon';
-import { createPortal } from 'react-dom';
-import {
-  Transition,
-  TRANSITION_LIFE_CIRCLE,
-  TRANSITION_STATUS,
-  TransitionCB,
-} from '../transition';
-import {
-  usePosition,
-  useResizerObserver,
-  useShowController,
-} from '../popover/popover.hooks';
-
-const rootName = getComponentClass('tooltip');
+import { Popover } from '../popover';
+import { RequiredPart } from '@tool-pack/types';
 
 export const Tooltip: React.FC<TooltipProps> = (props) => {
-  const {
-    disabled,
-    visible,
-    trigger,
-    placement,
-    children,
-    title,
-    className,
-    offset,
-    appendTo,
-    ...rest
-  } = props as RequiredPart<TooltipProps, keyof typeof defaultProps>;
-  const childrenRef = useRef<HTMLElement>(null);
-  const balloonRef = useRef<HTMLDivElement>();
-  const [refreshPosition, resetPlacement] = usePosition(
-    placement,
-    childrenRef,
-    balloonRef,
-    appendTo,
-    offset,
-  );
-  const show = useShowController(
-    disabled,
-    visible,
-    trigger,
-    children,
-    childrenRef,
-    balloonRef,
-    refreshPosition,
-  );
-
-  useResizerObserver(show, balloonRef, refreshPosition);
-  useResizeEvent(show, refreshPosition);
-
-  const Balloon = (
-    <WordBalloon
-      {...rest}
-      key="ballon"
-      placement={placement}
-      ref={balloonRef as React.Ref<HTMLDivElement>}
-      className={getClassNames(rootName, className)}>
-      {title}
-    </WordBalloon>
-  );
-
-  const onTransitionChange = useCallback<TransitionCB>(
-    (el, status, lifeCircle) => {
-      if (
-        [TRANSITION_STATUS.show, TRANSITION_STATUS.idle].includes(status) &&
-        TRANSITION_LIFE_CIRCLE.ready === lifeCircle
-      ) {
-        balloonRef.current = el as HTMLDivElement;
-        refreshPosition();
-        return;
-      }
-      if (
-        TRANSITION_STATUS.hide === status &&
-        TRANSITION_LIFE_CIRCLE.after === lifeCircle
-      ) {
-        resetPlacement();
-      }
-    },
-    [],
-  );
-
+  const { children, title, ...rest } = props as RequiredPart<
+    TooltipProps,
+    keyof typeof defaultProps
+  >;
   return (
-    <>
-      {React.cloneElement(children, {
-        ref: childrenRef,
-        key: children.key || rootName,
-      })}
-      {createPortal(
-        <Transition
-          name={rootName}
-          show={show}
-          on={onTransitionChange}
-          appear={null}>
-          {Balloon}
-        </Transition>,
-        appendTo(),
-      )}
-    </>
+    <Popover {...rest} content={title}>
+      {children}
+    </Popover>
   );
 };
 
@@ -109,6 +19,7 @@ const defaultProps = {
   placement: 'top',
   trigger: 'hover',
   offset: 10,
+  name: 'tooltip',
   appendTo: () => document.body,
 } satisfies Partial<TooltipProps>;
 Tooltip.defaultProps = defaultProps;

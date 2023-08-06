2ec27a2e8af161ae3980988ef9975ecad2e8cab0
dyh_a <xinzon32177@163.com>
2023-08-06 16:38:09 +0800

    feat(shared): 新增 PropsBase 类型

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -2,7 +2,7 @@ import type { SIZES } from './constant';
 import React from 'react';
 
 export type Size = (typeof SIZES)[number];
-export interface PropsBase<T = HTMLElement> {
+export interface PropsBase<T extends HTMLElement = HTMLElement> {
   attrs?: Partial<React.HTMLAttributes<T>>;
   children?: React.ReactNode;
   ref?: React.ForwardedRef<T>;

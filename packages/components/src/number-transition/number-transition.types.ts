904ad3b5bc6742dbc00644f6f50d48b4a12392eb
dyh_a <xinzon32177@163.com>
2023-08-06 16:43:20 +0800

    refactor(components/number-transition): 简化 props

--- packages/components/src/number-transition/number-transition.types.ts
+++ packages/components/src/number-transition/number-transition.types.ts
@@ -1,16 +1,14 @@
-import React from 'react';
-import type { TIMING_FNS } from '@pkg/shared';
+import type { PropsBase, TIMING_FNS } from '@pkg/shared';
 
-export type NumberTransitionProps = {
+export interface NumberTransitionProps
+  extends Omit<PropsBase<HTMLDivElement>, 'children'> {
   from?: number;
   to?: number;
   duration?: number;
   precision?: number | null;
   active?: boolean;
   timingFunction?: (typeof TIMING_FNS)[number];
-  ref?: React.ForwardedRef<HTMLDivElement>;
   onFinished?: () => void;
   format?: (value: number | string) => number | string;
-  attrs?: Partial<React.HTMLAttributes<HTMLDivElement>>;
   resetSignal?: unknown;
-};
+}

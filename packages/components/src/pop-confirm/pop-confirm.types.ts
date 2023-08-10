1153068f07af40f8fb51017c618d74bb4cc6e09e
dyh_a <xinzon32177@163.com>
2023-08-10 22:40:45 +0800

    refactor(components/pop-confirm): 平铺的 HTMLAttributes 属性收束到 attrs 属性

--- packages/components/src/pop-confirm/pop-confirm.types.ts
+++ packages/components/src/pop-confirm/pop-confirm.types.ts
@@ -1,14 +1,10 @@
 import React from 'react';
-import type { PopoverProps } from '../popover';
+import type { PopoverProps } from '~/popover';
 
-export type PopConfirmProps = Omit<
-  React.HTMLAttributes<HTMLElement>,
-  'children'
-> &
-  PopoverProps & {
-    confirmText?: string | null;
-    onConfirm?: () => boolean | Promise<void> | void;
-    cancelText?: string | null;
-    onCancel?: () => boolean | Promise<void> | void;
-    icon?: React.ReactNode;
-  };
+export interface PopConfirmProps extends PopoverProps {
+  confirmText?: string | null;
+  onConfirm?: () => boolean | Promise<void> | void;
+  cancelText?: string | null;
+  onCancel?: () => boolean | Promise<void> | void;
+  icon?: React.ReactNode;
+}

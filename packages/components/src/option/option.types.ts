c5d8b84b19e9e2ea5338a01decba55e367d23470
dyh_a <xinzon32177@163.com>
2023-08-11 16:49:20 +0800

    refactor(components/option): 平铺的 HTMLAttributes 属性收束到 attrs 属性

--- packages/components/src/option/option.types.ts
+++ packages/components/src/option/option.types.ts
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Size } from '@pkg/shared';
+import type { PropsBase, Size } from '@pkg/shared';
 
-export interface OptionProps {
+export interface OptionProps extends PropsBase {
   tag?: keyof HTMLElementTagNameMap;
   size?: Size;
   disabled?: boolean;
   expandable?: boolean;
   readonly?: boolean;
   icon?: React.ReactNode;
-  ref?: React.ForwardedRef<HTMLElement>;
-  attrs?: Partial<React.HTMLAttributes<HTMLElement>>;
-  children: React.ReactNode;
 }

a366a73f57e7ebae0fc2e950783a6a96b7bc3eef
dyh_a <xinzon32177@163.com>
2023-09-03 06:47:10 +0800

    feat(shared/observes): 新增 fromEventDelegation observe

---
+++ packages/shared/src/observes/fromEventDelegation.ts
@@ -0,0 +1,39 @@
+import { filter, fromEvent, type Observable } from 'rxjs';
+import { isChildHTMLElement } from '@tool-pack/dom';
+import { isFunction } from '@tool-pack/basic';
+
+/**
+ * 事件委托
+ */
+export function fromEventDelegation<K extends keyof HTMLElementEventMap>(
+  target: HTMLElement,
+  eventName: K,
+  options?: Omit<AddEventListenerOptions, 'signal'>,
+): Observable<HTMLElementEventMap[K]>;
+/**
+ * 事件委托
+ */
+export function fromEventDelegation<K extends keyof HTMLElementEventMap>(
+  getTarget: () => HTMLElement,
+  eventName: K,
+  options?: Omit<AddEventListenerOptions, 'signal'>,
+): Observable<HTMLElementEventMap[K]>;
+export function fromEventDelegation<K extends keyof HTMLElementEventMap>(
+  getEl: HTMLElement | (() => HTMLElement),
+  eventName: K,
+  options: Omit<AddEventListenerOptions, 'signal'> = {},
+): Observable<HTMLElementEventMap[K]> {
+  return fromEvent<HTMLElementEventMap[K]>(
+    document.documentElement,
+    eventName,
+    options,
+  ).pipe(
+    filter((e): boolean => {
+      // srcElement IE6-8
+      const target = (e.target || e.srcElement) as HTMLElement | null;
+      const el = isFunction(getEl) ? getEl() : getEl;
+      if (!target) return false;
+      return target === el || isChildHTMLElement(target, el);
+    }),
+  );
+}
--- packages/shared/src/observes/index.ts
+++ packages/shared/src/observes/index.ts
@@ -1,1 +1,2 @@
 export * from './fromOuterEvent';
+export * from './fromEventDelegation';

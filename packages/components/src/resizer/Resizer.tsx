3725c756fdacbf1eaa21dae41289d16c69c0f40e
dyh_a <xinzon32177@163.com>
2023-11-27 11:56:14 +0800

    perf(components/resizer): 简化 resizer 代码

--- packages/components/src/resizer/Resizer.tsx
+++ packages/components/src/resizer/Resizer.tsx
@@ -1,7 +1,7 @@
 import { getComponentClass, getElRealSize, useForwardRef } from '@pkg/shared';
+import { getClassNames, getSafeNum } from '@tool-pack/basic';
 import type { RequiredPart } from '@tool-pack/types';
 import type { ResizerProps } from './resizer.types';
-import { getClassNames } from '@tool-pack/basic';
 import { onDragEvent } from '@tool-pack/dom';
 import React, { useEffect } from 'react';
 
@@ -35,29 +35,25 @@ export const Resizer: React.FC<ResizerProps> = React.forwardRef<
         let h = 0;
         let w = 0;
 
-        onDown(() => {
-          [w, h] = getElRealSize(parent);
-        });
+        onDown(() => ([w, h] = getElRealSize(parent)));
+        const safeValue = (value: number) => getSafeNum(value, min, max) + 'px';
 
         const handlerMap: Record<
           typeof placement,
           Parameters<typeof onMove>[0]
         > = {
           bottom(_e, curr, _lastXY, down) {
-            const height = Math.min(max, Math.max(min, h + (curr.y - down.y)));
-            parent.style.height = height + 'px';
+            parent.style.height = safeValue(h + (curr.y - down.y));
           },
           top(_e, curr, _lastXY, down) {
-            const height = Math.min(max, Math.max(min, h - (curr.y - down.y)));
-            parent.style.height = height + 'px';
+            parent.style.height = safeValue(h - (curr.y - down.y));
           },
+          // eslint-disable-next-line perfectionist/sort-objects
           right(_e, curr, _lastXY, down) {
-            const width = Math.min(max, Math.max(min, w + (curr.x - down.x)));
-            parent.style.width = width + 'px';
+            parent.style.width = safeValue(w + (curr.x - down.x));
           },
           left(_e, curr, _lastXY, down) {
-            const width = Math.min(max, Math.max(min, w - (curr.x - down.x)));
-            parent.style.width = width + 'px';
+            parent.style.width = safeValue(w - (curr.x - down.x));
           },
         };
         onMove(handlerMap[placement]);

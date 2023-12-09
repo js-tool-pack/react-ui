0f22aaf649b92a2e306b20fb6119d5370b764d8f
dyh_a <xinzon32177@163.com>
2023-12-09 16:36:10 +0800

    feat(components): 新增 Slider 组件

--- packages/components/src/slider/__tests__/Slider.test.tsx
+++ packages/components/src/slider/__tests__/Slider.test.tsx
@@ -1,7 +1,12 @@
-// import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { testAttrs } from '~/testAttrs';
 import { Slider } from '..';
 
 describe('Slider', () => {
   testAttrs(Slider);
+
+  test('basic', () => {
+    const { container } = render(<Slider />);
+    expect(container).toMatchSnapshot();
+  });
 });

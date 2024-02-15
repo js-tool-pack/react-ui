18746b6eaa44981b8d43c0f7c86e7c0733a4deff
dyh_a <xinzon32177@163.com>
2024-02-15 23:36:37 +0800

    chore(dumi): 修复 css tree shaking demo 会导入全部 css 的问题

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,22 +1,33 @@
 import type { Project } from '@stackblitz/sdk';
 
 // 参考文档：https://d.umijs.org/config/runtime#modifystackblitzdata
-export function modifyStackBlitzData(memo: Project /*, props*/): Project {
-  memo.files[filename] = joinImportCss(memo.files[filename]);
+export function modifyStackBlitzData(
+  memo: Project,
+  props: { filename: string },
+): Project {
+  memo.files[filename] = joinImportCss(memo.files[filename], props.filename);
   return memo;
 }
 
-export function modifyCodeSandboxData(memo: {
-  files: Record<string, { content: string }>;
-}): typeof memo {
-  memo.files[filename].content = joinImportCss(memo.files[filename].content);
+export function modifyCodeSandboxData(
+  memo: {
+    files: Record<string, { content: string }>;
+  },
+  props: { filename: string },
+): typeof memo {
+  memo.files[filename].content = joinImportCss(
+    memo.files[filename].content,
+    props.filename,
+  );
   return memo;
 }
 
 const filename = 'App.tsx';
+const ignores = ['docs/guide/css-tree-shaking.tsx'];
 /**
  * 拼接 css 导入
  */
-function joinImportCss(content: string): string {
+function joinImportCss(content: string, filename: string): string {
+  if (ignores.includes(filename)) return content;
   return 'import "@tool-pack/react-ui/dist/index.css";\n' + content;
 }
